519471ff5662d141387e98c4aba01c3510972ae6
Izabelle Cesario <izabellecesario@hotmail.com>
2024-02-28 15:28:36 -0300

    js(funcoes): fim de 1.6.1

---
+++ index.js
@@ -0,0 +1,86 @@
+// //declaração de constantes
+// const nome = "Jose"
+// console.log(nome)
+// //nome = "Pedro"
+// const endereco = 'Rua B, 21'
+// console.log(endereco)
+// const sobrenome = `Bossini`
+// console.log(sobrenome)
+// console.log(`Me chamo ${nome}. Meu sobrenome e ${sobrenome}.`)
+
+// let nome = "Rodrigo"
+// console.log(nome)
+// nome = "Pedro"
+// console.log(nome)
+
+// let a = 2
+// console.log(typeof(a))
+// a = "abc"
+// console.log(typeof(a))
+// a = true
+// console.log(typeof(a))
+
+// var linguagem = "Javascript"
+// console.log(`Aprendendo ${linguagem}`)
+// var linguagem = `Java`
+// console.log(`Aprendendo ${linguagem}`)
+
+// var idade = 18
+// console.log(`Oi, ${nome}`)
+// if(idade >= 18){
+//     var nome = 'Joao'
+//     console.log(`Parabens, ${nome}. Voce pode dirigir`)
+// }
+// console.log(`Ate mais, ${nome}`)
+
+// const n1 = 2
+// const n2 = '3'
+// //coerção implicita
+// const n3 = n1 + n2
+// console.log(n3)
+// //coerção explicita
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+// operador de comparação
+// console.log(1 == 1)
+// console.log(1 == '1')
+// console.log(1 === 1)
+// console.log(1 === '1')
+// console.log(1 == true)
+// console.log(1 == [1])
+// console.log(null == null)
+// console.log(null == undefined)
+// console.log([] == [])
+
+// v1 = []
+// v1[0] = 3.4
+// console.log(v1.length)
+// v1[10] = 2
+// console.log(v1.length)
+// v1[2] = 'abc'
+// console.log(v1.length)
+// console.log(v1)
+
+// for(let i=0; i<v1.length;i++){
+//     console.log(v1[i])
+// }
+
+function hello(){
+    console.log('Oi')
+}
+hello()
+function hello(nome){
+    console.log('oi, ' + nome)
+}
+hello('Rodrigo')
+
+
+
+
+
+
+
+
+
+
